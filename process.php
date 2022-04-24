37154e2ab75bebee1188595079c6fe1a85766c04
Sebastián Meyer <sebastian@indomitux.com>
2022-04-24 08:04:39 -0300

    Cronograma

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 if($_POST['axn'] == 'contact_2' and isset($_POST['username']) != ""){
 	
-			$to = "consultas@somoscerveceros.com";
+			$to = "consultas@somoscerveceros.com.ar";
 			$from =  $_POST["email"];
 			$message = "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Name: </strong>".$_POST["username"]."<br />";
 			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email Address: </strong>".$_POST["email"]."<br />";
@@ -27,7 +27,7 @@ if($_POST['axn'] == 'contact_2' and isset($_POST['username']) != ""){
 }
 if($_POST['axn'] == 'contact_3' and isset($_POST['username']) != ""){
 	
-			$to = " consultas@somoscerveceros.com";
+			$to = " consultas@somoscerveceros.com.ar";
 			$from =  $_POST["email"];
 			$message = "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Name: </strong>".$_POST["username"]."<br />";
 			$message .= "&nbsp;&nbsp;&nbsp;&nbsp;<strong>Email Address: </strong>".$_POST["email"]."<br />";
